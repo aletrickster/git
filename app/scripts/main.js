e1745c1f5f88e2d414789b81125e63a3d86a7215
alena <alena>
2017-01-26 23:04:35 +0300

    start

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+/**
+ *  hellow-word
+ *  (c) alena <alena>
+ */
+
+'use strict';
+
+console.log('Hello, World!');
---
+++ tasks/wiredep.js
@@ -0,0 +1,18 @@
+/**
+ *  hellow-word
+ *  (c) alena <alena>
+ */
+
+var config = require('./.taskconfig');
+var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
+
+/**
+ * Injects Bower components into template files.
+ */
+gulp.task('wiredep', function()
+{
+  gulp.src(config.wiredep.entry)
+    .pipe(wiredep(config.wiredep.wiredep))
+    .pipe(gulp.dest(config.wiredep.output));
+});
